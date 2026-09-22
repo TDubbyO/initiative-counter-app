64554e3b825cd1faa5080ca0307e5dc1a40f07ce
Travis <travis.wo13@gmail.com>
2026-09-22 17:52:51 -0400

    connect js to html elements

---
+++ index.js
@@ -0,0 +1,6 @@
+const charName=document.getElementById('char-name')
+const initRoll=document.getElementById('init-roll')
+const addBtn=document.getElementById('add-btn')
+const combatantList=document.getElementById('combatant-list')
+
+console.log(charName, initRoll, addBtn, combatantList)
